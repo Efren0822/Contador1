564a5ec13145e17f243ae7b92763e088bac1d646
Efren <practicas038@gmail.com>
2024-03-08 15:55:04 -0600

    final commit

--- app/src/main/java/mx/edu/contador1/MainActivity.kt
+++ app/src/main/java/mx/edu/contador1/MainActivity.kt
@@ -12,13 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import mx.edu.contador1.Views.Contador
+import mx.edu.contador1.Views.LoteriaView
 import mx.edu.contador1.ui.theme.Contador1Theme
 import mx.edu.contador1.viewModels.ContadorViewModel
+import mx.edu.contador1.viewModels.LoteriaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel: ContadorViewModel by viewModels()
+        //val viewModel: ContadorViewModel by viewModels()
+        val ViewModel: LoteriaViewModel by viewModels()
         setContent {
             Contador1Theme {
                 // A surface container using the 'background' color from the theme
@@ -26,7 +29,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Contador(viewModel = viewModel)
+
+                    //Contador(viewModel = viewModel)
+                    LoteriaView(viewModel = ViewModel)
                 }
             }
         }
